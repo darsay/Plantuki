bb88f0b6ff8f77e1e9031c63a835b594e5322e12
Sergio Sánchez <serjsa385@gmail.com>
2021-11-04 17:25:18 +0100

    PlantController & PlantBehaviour
    - Added basics stats for Plantuki and methods to change them in PlantBehaviour.
    
    - Added stats check and automatic lower every x seconds in PlantController

--- Plantuki/Assets/Scripts/Plant/PlantBehaviour.cs
+++ Plantuki/Assets/Scripts/Plant/PlantBehaviour.cs
@@ -11,23 +11,92 @@ public class PlantBehaviour : MonoBehaviour
     public static PlantBehaviour instance;
 
     // Plant needs with getters and setters
-    public float wetness { get; set; }
-    public float satiety { get; set; }
-    public float cleanliness { get; set; }
-    public float lightness { get; set; }
+    public float wetness { get; private set; }
+    public float satiety { get; private set; }
+    public float cleanliness { get; private set; }
+    public float lightness { get; private set; } // Light needs to be around 60 to be OK
 
     private void Start()
     {
         instance = this;
 
+        // Se inicializa a 100, debería cambiarse para guardar el estado entre partidas
         this.wetness = 100f;
         this.satiety = 100f;
         this.cleanliness = 100f;
         this.lightness = 100f;
     }
 
+    /////////////////////////////////////////////////////////////
+    /// INCREASE STATS
+    /////////////////////////////////////////////////////////////
+    #region  Increase stats
+    public void GiveWater(float f)
+    {
+        if (wetness + f <= 100)
+            wetness += f;
+    }
+    public void GiveFood(float f)
+    {
+        if (satiety + f <= 100)
+            satiety += f;
+    }
+    public void GiveClean(float f)
+    {
+        if (cleanliness + f <= 100)
+            cleanliness += f;
+    }
+
+    public void GiveLight(float f)
+    {
+        if (lightness + f <= 100)
+            lightness += f;
+    }
+    #endregion
 
 
+    /////////////////////////////////////////////////////////////
+    /// DECREASE STATS
+    /////////////////////////////////////////////////////////////
+    #region  Decrease stats
+    public void DecreaseAll(float wet, float sat, float clean, float light)
+    {
+        if (wetness >= wet)
+            wetness -= wet;
+
+        if (satiety >= sat)
+            satiety -= sat;
+
+        if (cleanliness >= clean)
+            cleanliness -= clean;
+
+        if (lightness >= light)
+            lightness -= light;
+    }
+
+    public void MakeDry(float f)
+    {
+        if (wetness >= f)
+            wetness -= f;
+    }
+
+    public void MakeHungry(float f)
+    {
+        if (satiety >= f)
+            satiety -= f;
+    }
+
+    public void MakeDirty(float f)
+    {
+        if (cleanliness >= f)
+            cleanliness -= f;
+    }
 
 
+    public void MakeDark(float f)
+    {
+        if (lightness >= f)
+            lightness -= f;
+    }
+    #endregion
 }
--- Plantuki/Assets/Scripts/Plant/PlantController.cs
+++ Plantuki/Assets/Scripts/Plant/PlantController.cs
@@ -6,24 +6,51 @@ using System;
 
 public class PlantController : MonoBehaviour
 {
-    DateTime dateTime;
+    DateTime dateTime; // Tests for storing the date & time
 
-    private const float CHECK_STATUS_TIMER = 1f;
-    private const float LOWER_SATIETY_TIMER = 0.5f;
+    // Timers, in seconds
+    [SerializeField] private float checkStatusSeconds = 1f;
+    [SerializeField] private float lowerStatsSeconds = 1f;
+
+    [SerializeField] private float amoutLoweredStats = 1f;
 
 
     private void Start()
     {
-        StartCoroutine(MakeHungry(LOWER_SATIETY_TIMER));
-        InvokeRepeating("CheckSatiety", CHECK_STATUS_TIMER, CHECK_STATUS_TIMER);
+        // Functions invoked every x seconds.
+        InvokeRepeating("CheckStats", 0, checkStatusSeconds); 
+        InvokeRepeating("LowerStats", 0, lowerStatsSeconds);
     }
 
+   
+
     /*
-     *      CHECK STATS
+     *    LOWER STATS (for any reason)
      */
+    private void LowerStats()
+    {
+        // Lowers Plantuki's stats each x time.
+        PlantBehaviour.instance.MakeHungry(amoutLoweredStats);
+        PlantBehaviour.instance.MakeDry(amoutLoweredStats);
+        PlantBehaviour.instance.MakeDirty(amoutLoweredStats);
+        PlantBehaviour.instance.MakeDark(amoutLoweredStats);
+    }
+
+
+    /*
+    *      CHECK STATS
+    */
+    private void CheckStats()
+    {
+        CheckSatiety();
+        CheckWetness();
+        CheckCleanliness();
+        CheckLightness();
+    }
+
     private void CheckSatiety()
     {
-        switch(PlantBehaviour.instance.satiety)
+        switch (PlantBehaviour.instance.satiety)
         {
             case 100f:
                 Debug.Log("Your plant is full");
@@ -38,21 +65,56 @@ public class PlantController : MonoBehaviour
                 Debug.Log("Your plant starving to death!");
                 break;
             case 0f:
-                Debug.Log("Your plant starved");
+                Debug.Log("Your plant starved.");
                 break;
         }
     }
-
-
-    /*
-     *      LOWER STATS (for any reason)
-     */
-
-    private IEnumerator MakeHungry(float points_lost)
+    private void CheckWetness()
     {
-        yield return new WaitForSeconds(LOWER_SATIETY_TIMER);
-
-        if(PlantBehaviour.instance.satiety > 0)
-            PlantBehaviour.instance.satiety -= points_lost;
+        switch (PlantBehaviour.instance.wetness)
+        {
+            case 100f:
+                Debug.Log("Your plant is wet and happy");
+                break;
+            case 40f:
+                Debug.Log("Your plant is starting to feel thirsty");
+                break;
+            case 10f:
+                Debug.Log("Your plant is very thirsty!");
+                break;
+            case 0f:
+                Debug.Log("Your plant died of thirst.");
+                break;
+        }
+    }
+    private void CheckCleanliness()
+    {
+        switch (PlantBehaviour.instance.cleanliness)
+        {
+            case 100f:
+                Debug.Log("Your plant is clean");
+                break;
+            case 50f:
+                Debug.Log("Your plant is slightly dirty");
+                break;
+            case 5f:
+                Debug.Log("Your plant really needs a cleanup");
+                break;
+        }
+    }
+    private void CheckLightness()
+    {
+        switch (PlantBehaviour.instance.lightness)
+        {
+            case 100f:
+                Debug.Log("Your plant has too much light");
+                break;
+            case 50f:
+                Debug.Log("Your plant is correctly iluminated");
+                break;
+            case 15f:
+                Debug.Log("Your plant needs light!");
+                break;
+        }
     }
 }
