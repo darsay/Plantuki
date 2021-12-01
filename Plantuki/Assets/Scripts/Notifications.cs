86428288ec55f2f94b72347db07089c17be2a80f
DavidGanfornina <73431730+DavidGanfornina@users.noreply.github.com>
2021-12-01 16:43:03 +0100

    Notificaciones

--- Plantuki/Assets/Scripts/Notifications.cs
+++ Plantuki/Assets/Scripts/Notifications.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Notifications.Android;
@@ -5,17 +6,56 @@ using UnityEngine;
 
 public class Notifications : MonoBehaviour
 {
+
+    public static Notifications SharedInstance;
+
+
+    private void Awake()
+    {
+        if (SharedInstance != null)
+        {
+            SharedInstance = this;
+        }
+    }
+
+    private int id;
     // Start is called before the first frame update
     void Start()
     {
         var channel = new AndroidNotificationChannel()
         {
-            Id = "channel_id",
+            Id = "channel_id1",
             Name = "Default Channel",
             Importance = Importance.Default,
             Description = "Generic notifications",
         };
         AndroidNotificationCenter.RegisterNotificationChannel(channel);
+        
+        var channel2 = new AndroidNotificationChannel()
+        {
+            Id = "channel_id2",
+            Name = "Default Channel",
+            Importance = Importance.Default,
+            Description = "Generic notifications",
+        };
+        AndroidNotificationCenter.RegisterNotificationChannel(channel2);
+    }
+
+    public void sendNotification()
+    {
+        var notification = new AndroidNotification();
+        notification.Title = "PLANTUKI TIENE SED";
+        notification.Text = "Hace mucho que no le das agua, vuelve pronto";
+        notification.FireTime = DateTime.Now.AddMinutes(1);
+        notification.SmallIcon = "plant";
+        notification.LargeIcon = "plant";
+        
+        id = AndroidNotificationCenter.SendNotification(notification, "channel_id1");
+    }
+
+    public void cancelNotification()
+    {
+        AndroidNotificationCenter.CancelNotification(id);
     }
 
     // Update is called once per frame
